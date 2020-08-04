968fe5e38bbdd13bc21c28e2cd043b32fc5c25d1
Sean Taylor <silverwolf4350@gmail.com>
2020-08-04 22:23:09 +0000

    Changed IdentityFile to replace the line.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -7,6 +7,7 @@ file_line { 'Turn off passwd auth':
 }
 
 file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/holberton'
+  path =>  '/etc/ssh/ssh_config',
+  line =>  '    IdentityFile ~/.ssh/holberton'
+  match => 'IdentityFile'
 }
