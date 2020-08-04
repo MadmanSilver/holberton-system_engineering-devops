ddcce3ad9966600c38ad1b52c916bd7cb7b01d55
Sean Taylor <silverwolf4350@gmail.com>
2020-08-04 21:54:22 +0000

    Created all files for advanced tasks.

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Sets ssh config values
+include stdlib
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    BatchMode yes',
+  match => 'BatchMode'
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/holberton'
+}
