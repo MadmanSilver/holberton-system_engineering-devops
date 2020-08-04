4ff9c739e4e8fc2514e30235463088388b6ed71b
Sean Taylor <silverwolf4350@gmail.com>
2020-08-04 22:27:22 +0000

    Changed to not replace the line.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 # Sets ssh config values
 include stdlib
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    BatchMode yes',
-  match => 'BatchMode'
+  path => '/etc/ssh/ssh_config',
+  line => '    BatchMode yes',
 }
 
 file_line { 'Declare identity file':
   path =>  '/etc/ssh/ssh_config',
   line =>  '    IdentityFile ~/.ssh/holberton'
-  match => 'IdentityFile'
 }
