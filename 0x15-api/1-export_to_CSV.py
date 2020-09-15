d1d0b3314685cb45297112e2c3a2b224b437a9ff
Sean Taylor <silverwolf4350@gmail.com>
2020-09-15 02:45:23 +0000

    Swapped from manual csv formatting to csv module.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,19 +3,16 @@
 
 import requests
 from sys import argv
+import csv
 
 if __name__ == "__main__":
-    csv = ""
-
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}\
 '.format(argv[1])).json()
     todos = requests.get('https://jsonplaceholder.typicode.com/users/{}\
 /todos'.format(argv[1])).json()
 
-    for todo in todos:
-        csv += '"{}","{}","{}","{}"\n'.format(argv[1], user.get('name'),
-                                              todo.get('completed'),
-                                              todo.get('title'))
-
     with open('{}.csv'.format(argv[1]), 'w') as f:
-        f.write(csv)
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for todo in todos:
+            writer.writerow([argv[1], user.get('name'), todo.get('completed'), todo.get('title')])
