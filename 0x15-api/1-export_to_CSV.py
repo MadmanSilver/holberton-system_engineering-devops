1c485d85969130c38fdc9cafa1caa3a601ac01ca
Sean Taylor <silverwolf4350@gmail.com>
2020-09-15 03:00:50 +0000

    Changed to use 'username' instead of 'name'.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Gets info about an employee """
 
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == "__main__":
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}\
@@ -12,7 +12,9 @@ if __name__ == "__main__":
 /todos'.format(argv[1])).json()
 
     with open('{}.csv'.format(argv[1]), 'w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
 
         for todo in todos:
-            writer.writerow([argv[1], user.get('name'), todo.get('completed'), todo.get('title')])
+            writer.writerow([argv[1], user.get('username'),
+                             todo.get('completed'), todo.get('title')])
